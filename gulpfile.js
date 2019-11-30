1a1bb8e92567894363b00be76775e8a66e3cdbbf
Ivan Nasanovich <37714995+Uolary@users.noreply.github.com>
2019-11-30 12:41:58 +0300

    added and configured gulp.js

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	prefixer = require('gulp-autoprefixer'),
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass'),
+	cssMin = require('gulp-minify-css'),
+	rimRaf = require('rimraf'),
+	rename = require('gulp-rename'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload;
+
+const path = {
+	build: {
+		html: 'build',
+		js: 'build/js',
+		css: 'build/css'
+	},
+	src: {
+		html: 'src/*.html',
+		js: 'src/js/*.js',
+		style: 'src/style/main.scss'
+	},
+	watch: {
+		html: 'src/*.html',
+		js: 'src/js/**/*.js',
+		style: 'src/style/**/*.scss'
+	},
+	clean: 'build'
+};
+
+gulp.task('webserver', function() {
+	browserSync({
+		server: {
+			baseDir: 'build'
+		}
+	});
+});
+
+gulp.task('html:build', function() {
+	return gulp.src(path.src.html)
+		.pipe(gulp.dest(path.build.html))
+		.pipe(reload({stream: true}))
+})
+
+gulp.task('js:build', function() {
+	return gulp.src(path.src.js)
+		.pipe(uglify())
+		.pipe(rename('scripts.min.js'))
+		.pipe(gulp.dest(path.build.js))
+		.pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function() {
+	return gulp.src(path.src.style)
+		.pipe(sass())
+		.pipe(prefixer())
+		.pipe(cssMin())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(path.build.css))
+		.pipe(reload({stream: true}));
+});
+
+gulp.task('clean', function(cb) {
+	rimRaf(path.clean, cb);
+});
+
+gulp.task('watch', function() {
+	gulp.watch(path.watch.html, gulp.parallel('html:build'));
+	gulp.watch(path.watch.js, gulp.parallel('js:build'));
+	gulp.watch(path.watch.style, gulp.parallel('style:build'));	
+});
+
+gulp.task('build', gulp.parallel('html:build', 'js:build', 'style:build'));
+
+gulp.task('default', gulp.parallel('build', 'webserver', 'watch'));
